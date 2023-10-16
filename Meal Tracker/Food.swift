34582dc90e0b0c62f6edbe96cfe577064065a882
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-10-16 06:49:05 -0600

    good to go

---
+++ Meal Tracker/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Austin Dobberfuhl on 10/13/23.
+//
+
+import Foundation
+
+
+struct Food {
+    var name: String
+    var description: String
+}
